f409a9226dd205cc044e0e2688c4af320f9935b2
Shreejal Dhungana <dshreejal@gmail.com>
2023-09-01 17:12:10 +0545

    card design initial

---
+++ src/components/Card.tsx
@@ -0,0 +1,47 @@
+import React, { FC } from "react";
+import { Button } from "./ui/button";
+import { AiOutlineShoppingCart } from "react-icons/ai";
+
+interface CardProps {
+  title: string;
+  price: number;
+  image: string;
+}
+
+const Card: FC<CardProps> = ({ title, price, image }) => {
+  return (
+    <div className="group border-gray-100/30 flex w-full max-w-xs flex-col self-center overflow-hidden rounded-lg border bg-white shadow-xl m-5">
+      <a
+        className="relative mx-3 mt-3 flex h-60 overflow-hidden rounded-xl"
+        href="#"
+      >
+        <img
+          className="peer absolute top-0 right-0 h-full w-full object-cover"
+          src={image}
+          alt="product image"
+        />
+      </a>
+      <div className="mt-4 px-5 pb-5">
+        <a href="#" className="block overflow-hidden h-16">
+          <h5 className="text-xl tracking-tight text-black overflow-hidden overflow-ellipsis whitespace-nowrap">
+            {title}
+          </h5>
+        </a>
+        <div className="mt-2 mb-5 flex items-center justify-between">
+          <p>
+            <span className="text-3xl font-bold text-black">${price}</span>
+          </p>
+        </div>
+        <div className="flex items-center justify-between">
+          <Button className="flex items-center justify-center gap-3 px-5 py-2.5 text-center text-sm font-medium text-white focus:outline-none focus:ring-4 ">
+            <AiOutlineShoppingCart size={20} />
+            Add to cart
+          </Button>
+          <Button variant="link">View</Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
