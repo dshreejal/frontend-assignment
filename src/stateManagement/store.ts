c50c21c5966d30a32b3b0953d0464fa6e162b81b
Shreejal Dhungana <dshreejal@gmail.com>
2023-09-01 21:26:45 +0545

    store changes

--- src/stateManagement/store.ts
+++ src/stateManagement/store.ts
@@ -1,9 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
+import CartSlice from "./slices/CartSlice";
 
 const store = configureStore({
   reducer: {
-    // TODO: add reducers
+    cart: CartSlice,
   },
 });
 
 export default store;
+
+// Define RootState type based on the store configuration
+export type RootState = ReturnType<typeof store.getState>;
