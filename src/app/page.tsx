1f909601a16dfd1177225f0ad34a16a3a53f2503
Shreejal Dhungana <dshreejal@gmail.com>
2023-09-01 20:47:54 +0545

    used react query to fetch homepage data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
+"use client";
 import Banner from "@/components/Banner";
 import Card from "@/components/Card";
 import ProductCategory from "@/components/ProductCategory";
 import { ProductsProps } from "@/types";
 import Image from "next/image";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import CardSkeleton from "@/components/CardSkeleton";
 
 const categories = [
   "electronics",
@@ -12,23 +16,48 @@ const categories = [
 ];
 
 const fetchdata = async (category: string) => {
-  const res = await fetch(
+  const res = await axios.get(
     `https://fakestoreapi.com/products/category/${category}`
   );
 
-  //add a promise to slow the data fetching
-  await new Promise((resolve) => setTimeout(resolve, 250));
+  //promise to slow down the fetching
+  await new Promise((resolve) => setTimeout(resolve, 1000));
 
-  const data = await res.json();
+  const data = await res.data;
   return data;
 };
 
-export default async function Home() {
-  const ElectronicData = await fetchdata("electronics");
-  const jeweleryData = await fetchdata("jewelery");
-  const mensClothingData = await fetchdata("men's clothing");
-  const womenClothingData = await fetchdata("women's clothing");
+export default function Home() {
+  const { data: ElectronicData, isLoading } = useQuery(["electronics"], () =>
+    fetchdata("electronics")
+  );
+  const { data: jeweleryData } = useQuery(["jewelery"], () =>
+    fetchdata("jewelery")
+  );
+  const { data: mensClothingData } = useQuery(["men's clothing"], () =>
+    fetchdata("men's clothing")
+  );
+  const { data: womenClothingData } = useQuery(["women's clothing"], () =>
+    fetchdata("women's clothing")
+  );
 
+  if (isLoading) {
+    const numberOfSkeletons = 20;
+    const skeletons = Array.from({ length: numberOfSkeletons }, (_, index) => (
+      <CardSkeleton key={index} />
+    ));
+
+    return (
+      <>
+        <Banner />
+        <div className="flex justify-center items-center">
+          <div className="m-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4">
+            {skeletons}
+          </div>
+        </div>
+      </>
+    );
+  }
   return (
     <>
       <Banner />
