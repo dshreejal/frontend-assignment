e33f60fe99bf98ca9b5d78a3b0acfa653268356b
Shreejal Dhungana <dshreejal@gmail.com>
2023-09-01 23:00:16 +0545

    configured search functionality

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,11 @@
 "use client";
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import { Input } from "@/components/ui/input";
 import { Button } from "./ui/button";
 import { AiOutlineShoppingCart } from "react-icons/ai";
 import { BiStore } from "react-icons/bi";
 import { useSelector } from "react-redux";
+import { useRouter } from "next/navigation";
 
 import Link from "next/link";
 import { RootState } from "@/stateManagement/store";
@@ -12,9 +13,18 @@ import { RootState } from "@/stateManagement/store";
 interface NavbarProps {}
 
 const Navbar: FC<NavbarProps> = ({}) => {
+  const router = useRouter();
+  const [searchValue, setSearchValue] = useState("");
+
   const cart = useSelector((state: RootState) => {
     return state.cart;
   });
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchValue(event.target.value);
+    router.push(`/search/${event.target.value}`);
+  };
+
   return (
     <header className="text-slate-700 w-full fixed z-50 bg-white mx-auto flex flex-col overflow-hidden px-4 py-4 lg:flex-row lg:items-center shadow-2xl top-0">
       <Link
@@ -50,7 +60,13 @@ const Navbar: FC<NavbarProps> = ({}) => {
         aria-label="Header Navigation"
         className="peer-checked:pt-8 peer-checked:max-h-60 flex max-h-0 w-full flex-col items-center overflow-hidden transition-all lg:ml-24 lg:max-h-full lg:flex-row lg:py-2"
       >
-        <Input type="text" placeholder="Search" className="w-2/3 ml-1" />
+        <Input
+          type="text"
+          placeholder="Search"
+          className="w-2/3 ml-1"
+          value={searchValue} // Step 4: Set input value from state
+          onChange={handleInputChange}
+        />
         <hr className="mt-4 w-full lg:hidden" />
         <div className="my-4 flex items-center space-x-6 space-y-2 lg:my-0 lg:ml-auto lg:space-x-8 lg:space-y-0">
           <Link
